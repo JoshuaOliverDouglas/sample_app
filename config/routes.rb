591a56cf83eb0bce6fbdbb534bed46fc74afbeec
Joshua <joshuaoliverdouglas@gmail.com>
2020-01-12 18:43:01 +0000

    Finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pagges/home'
-  get 'static_pagges/help'
-  root 'application#hello'
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
